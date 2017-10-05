39406d79b1415e0d07c0d6a7967ea40d6ea476ce
inkfan <781928758@qq.com>
2017-10-05 22:06:46 +0800

    Hello-World

---
+++ Hello-world/Helloworld/app/src/main/java/com/feicuiedu/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.feicuiedu.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // abc
+        // 这是一次新的提交
+        // 这是第二次提交
+    }
+}
